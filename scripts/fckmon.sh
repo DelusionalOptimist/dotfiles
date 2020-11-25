713e01f7d93bcc5b121ea07f4fab30040b530ae1
DelusionalOptimist <54525605+DelusionalOptimist@users.noreply.github.com>
2020-11-26 01:47:31 +0530

    add scripts

---
+++ scripts/fckmon.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync & sleep 2
+xrandr --addmode DP1 1368x768_60.00 & sleep 2
+xrandr --output DP1 --mode 1368x768_60.00 & sleep 2
+xrandr --output DP1 --left-of eDP1
